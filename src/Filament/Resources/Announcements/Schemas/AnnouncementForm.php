290c674d3272f1c49d50411624d8db503af887ab
roble <renanroble@gmail.com>
2026-09-15 20:17:25 +0100

    feat: restructure AnnouncementForm to enhance layout and improve component organization

--- src/Filament/Resources/Announcements/Schemas/AnnouncementForm.php
+++ src/Filament/Resources/Announcements/Schemas/AnnouncementForm.php
@@ -5,6 +5,8 @@ namespace Modules\Announcements\Filament\Resources\Announcements\Schemas;
 use Filament\Forms\Components\DateTimePicker;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Group;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class AnnouncementForm
@@ -12,31 +14,59 @@ class AnnouncementForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
-            ->columns(2)
+            ->columns(3)
             ->components([
-                RichEditor::make('text')
-                    ->required()
-                    ->toolbarButtons([
-                        ['bold', 'italic', 'underline', 'strike'],
-                        ['link'],
-                        ['bulletList', 'orderedList'],
-                    ])
-                    ->columnSpanFull(),
-                Toggle::make('is_active')
-                    ->default(false),
-                Toggle::make('is_dismissable')
-                    ->default(false),
-                Toggle::make('show_on_frontend')
-                    ->label(__('announcements::filament.show_on_frontend'))
-                    ->default(true),
-                Toggle::make('show_on_dashboard')
-                    ->label(__('announcements::filament.show_on_dashboard'))
-                    ->default(true),
-                DateTimePicker::make('starts_at')
-                    ->nullable(),
-                DateTimePicker::make('ends_at')
-                    ->nullable()
-                    ->rules(['nullable', 'after_or_equal:starts_at']),
+                Group::make([
+                    Section::make(__('Message'))
+                        ->description(__('Shown in the banner at the top of the page.'))
+                        ->schema([
+                            RichEditor::make('text')
+                                ->hiddenLabel()
+                                ->required()
+                                ->toolbarButtons([
+                                    ['bold', 'italic', 'underline', 'strike'],
+                                    ['link'],
+                                    ['bulletList', 'orderedList'],
+                                ]),
+                        ]),
+                ])->columnSpan(['lg' => 2]),
+
+                Group::make([
+                    Section::make(__('Publish'))
+                        ->schema([
+                            Toggle::make('is_active')
+                                ->label(__('Active'))
+                                ->default(false),
+
+                            Toggle::make('is_dismissable')
+                                ->label(__('Visitors can dismiss it'))
+                                ->default(false),
+                        ]),
+
+                    Section::make(__('Schedule'))
+                        ->description(__('Leave empty to show it while active.'))
+                        ->schema([
+                            DateTimePicker::make('starts_at')
+                                ->label(__('Starts'))
+                                ->nullable(),
+
+                            DateTimePicker::make('ends_at')
+                                ->label(__('Ends'))
+                                ->nullable()
+                                ->rules(['nullable', 'after_or_equal:starts_at']),
+                        ]),
+
+                    Section::make(__('Audience'))
+                        ->schema([
+                            Toggle::make('show_on_frontend')
+                                ->label(__('announcements::filament.show_on_frontend'))
+                                ->default(true),
+
+                            Toggle::make('show_on_dashboard')
+                                ->label(__('announcements::filament.show_on_dashboard'))
+                                ->default(true),
+                        ]),
+                ])->columnSpan(['lg' => 1]),
             ]);
     }
 }
